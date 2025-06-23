ff65f05a2dc703afb8d120f658025335e13b58dd
Rishabh Nauhwar <rishabhnauhowar@gmail.com>
2025-06-23 14:03:56 +0000

    why getting the error?

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  pageExtensions: ['api.ts', 'api.js'],
   typescript: {
     ignoreBuildErrors: true,
   },
